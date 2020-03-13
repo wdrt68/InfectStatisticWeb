a8dfdad6bb769f2735ff12483649e4d3798e6dc1
1766338978 <60637171+1766338978@users.noreply.github.com>
2020-03-13 19:18:15 +0800

    echarts 嵌入完善

--- testweb/dataview/lineecharts.php
+++ testweb/dataview/lineecharts.php
@@ -1,8 +1,8 @@
 <?php
 	require "public_function.php";
 	dbInit();
-	$area = '上海';
-	$sql = "select * from province where p_name='{$area}'";
+	$province = '上海';
+	$sql = "select * from province where p_name='{$province}'";
 	$con->query("SET NAMES utf8");
 	$datas = p_fetchAll($sql);
 	//print_r($datas);
